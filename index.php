e333296ffdf0b9c8f9819e3f22d450bf3d90d7dd
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-05-02 00:32:57 -0300

    criando uma rota

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+     require 'vendor/autoload.php';
+
+     // Criando objeto para definição das rotas
+     $app = new \Slim\App;
+
+     // criando uma rota e passando uma função para ela
+     $app->get('/postagens', function(){
+          echo "Lista de postagens: <BR>";
+     });
+
+     // Executando a aplicação
+
+     $app->run();
