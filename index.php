073dd01f994be557307bff5150bf36132e438f17
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-05-02 23:53:55 -0300

    parametros opcionais

--- index.php
+++ index.php
@@ -5,13 +5,18 @@
      $app = new \Slim\App;
 
      // criando rotas e passando uma função para ela
-     $app->get('/postagens', function(){
+     $app->get('/postagens2', function(){
           echo "Lista de postagens: <BR>";
      });
      
      $app->get('/usuarios[/{id}]', function($request, $response){
           $id = $request->getAttribute('id');
           echo "Lista de usuários ou ID: {$id}<BR>";
+     });
+     $app->get('/postagens[/{ano}[/{mes}]]', function($request, $response){
+          $ano = $request->getAttribute('ano');
+          $mes = $request->getAttribute('mes');
+          echo "Lista de postagens Ano: {$ano} mes: {$mes}";
      });
      // Executando a aplicação
 
